faf0406dec5424e9568ff29e264d33aec5beea76
Verna Koskinen <darth.kipsu@gmail.com>
2014-09-17 09:58:33 +0300

    korjaa tehtävä 3.3 Esitä erilaiset vaihtoehdot arrayssa

--- viikko03-tira3.3/src/Main.java
+++ viikko03-tira3.3/src/Main.java
@@ -1,35 +1,45 @@
 import java.util.*;
+import org.omg.CORBA.INTERNAL;
 
 public class Main {
 
-	private static List<List<Integer>> vaihtoehdot;
+	private static List<int[]> vaihtoehdot;
 
     public static int nopanheitot(int x) {
-		vaihtoehdot = new ArrayList<List<Integer>>();
-		heitot(x, new ArrayList<Integer>());
+		vaihtoehdot = new ArrayList<int[]>();
+		heitot(x, 0, new int[x]);
 		return vaihtoehdot.size();
     }
 
-	private static void heitot(int x, List<Integer> vaihtoehto) {
+	private static void heitot(int x, int i, int[] vaihtoehto) {
 		if (x == 0) {
 			vaihtoehdot.add(vaihtoehto);
 			return;
 		}
-		if (x >= 6) lisaa(6, vaihtoehto, x);
-		if (x >= 5) lisaa(5, vaihtoehto, x);
-		if (x >= 4) lisaa(4, vaihtoehto, x);
-		if (x >= 3) lisaa(3, vaihtoehto, x);
-		if (x >= 2) lisaa(2, vaihtoehto, x);
-		if (x >= 1) lisaa(1, vaihtoehto, x);
+		if (x >= 6) lisaa(6, i, vaihtoehto, x);
+		if (x >= 5) lisaa(5, i, vaihtoehto, x);
+		if (x >= 4) lisaa(4, i, vaihtoehto, x);
+		if (x >= 3) lisaa(3, i, vaihtoehto, x);
+		if (x >= 2) lisaa(2, i, vaihtoehto, x);
+		if (x >= 1) lisaa(1, i, vaihtoehto, x);
 	}
 
-	private static void lisaa(int silmaluku, List<Integer> vaihtoehto, int x) {
-		vaihtoehto.add(silmaluku);
-		heitot(x-silmaluku, vaihtoehto);
+	private static void lisaa(int silmaluku, int i, int[] vaihtoehto, int x) {
+		int[] uusiVaihtoehto = vaihtoehto.clone();
+		uusiVaihtoehto[i] = silmaluku;
+		heitot(x-silmaluku, i+1, uusiVaihtoehto);
 	}
     
     public static void main(String[] args) {
         System.out.println(nopanheitot(4));
+		System.out.println(Arrays.toString(vaihtoehdot.get(0)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(1)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(2)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(3)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(4)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(5)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(6)));
+		System.out.println(Arrays.toString(vaihtoehdot.get(7)));
         System.out.println(nopanheitot(2));
         System.out.println(nopanheitot(3));
         System.out.println(nopanheitot(1));
