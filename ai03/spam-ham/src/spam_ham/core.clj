3f0511120e37ee8436a99fb622998c0e6de81692
Verna Koskinen <darth.kipsu@gmail.com>
2016-02-02 19:08:22 +0200

    predict the likelihood of an email being spam

--- ai03/spam-ham/src/spam_ham/core.clj
+++ ai03/spam-ham/src/spam_ham/core.clj
@@ -7,14 +7,38 @@
   (let [item (str/split (str/trim line) #" ")]
     (assoc a-map (second item) (/ (Integer/parseInt (first item)) divisor))))
 
+(defn- conj-words [a-list line]
+  (apply conj a-list (str/split (str/trim line) #" ")))
+
 (defn- read-word-counts [file divisor]
   (with-open[rdr (reader file)]
     (reduce (partial to-map divisor) {} (line-seq rdr))))
 
+(defn- mail-words [file]
+  (let [lines (str/split-lines (slurp file))]
+    (reduce conj-words '() lines)))
+
+(defn- prob [p-list word]
+  (let [p (get p-list word)]
+    (if (nil? p) 1/100000
+      p)))
+
+(defn- prob-spam-ham [word spam ham]
+  (Math/log (/ (prob spam word) (prob ham word))))
+
+(defn- prob-with-word [spam ham probability word]
+  (+ probability (prob-spam-ham word spam ham)))
+
+(defn- likely-spam [file spam ham]
+  (let [starting-prob (Math/log (/ 1/2 1/2))
+        odds (reduce (partial prob-with-word spam ham) starting-prob (mail-words file))
+        spam-amount (/ odds (+ 1 odds))]
+    (if (< 1 spam-amount) (- spam-amount 1)
+      spam-amount)))
 
 (defn -main
   "Count how likely a message is spam based on spam and ham message word frequencies."
   [& args]
   (let [spam (read-word-counts "resources/spamcount.txt" 75268)
         ham (read-word-counts "resources/hamcount.txt" 290673)]
-    (println (count spam)  (count ham))))
+    (println (likely-spam (first args) spam ham))))
