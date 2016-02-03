59bc28f29d6d7ba3e7a92cba811b15ea09f497f5
Verna Koskinen <darth.kipsu@gmail.com>
2016-02-03 11:16:16 +0200

    allow more than one queen for queen score

--- ai03/Shakki/src/main/java/scoring/QueenScore.java
+++ ai03/Shakki/src/main/java/scoring/QueenScore.java
@@ -7,21 +7,21 @@ public class QueenScore extends PieceScore {
     
     private static final double BASE_SCORE = 9;
     
-    private boolean hasQueen = false;
-    private Coordinate coordinate = null;
+    private int queenCount = 0;
+    private Coordinate[] coordinates = new Coordinate[7];
 
     public QueenScore(int sideCoefficient, Position p) {
         super(sideCoefficient, p);
     }
 
     public void addPiece(int x, int y) {
-        hasQueen = true;
-        coordinate = new Coordinate(x, y);
+        queenCount++;
+        coordinates[queenCount] = new Coordinate(x, y);
     }
 
     public double getScore() {
         double score = 0;
-        if (hasQueen) score += BASE_SCORE;
+        score += queenCount * BASE_SCORE;
         return this.sideCoefficient * score;
     }
 }
