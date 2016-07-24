5503c40ed6ca08e18e2cedaa4765e5e47b50efb3
Verna Koskinen <darth.kipsu@gmail.com>
2016-07-24 14:10:38 +0300

    add 2d tab;les fast sum calculation

--- practice/src/algorithms/SumTables.java
+++ practice/src/algorithms/SumTables.java
@@ -12,6 +12,7 @@ public class SumTables {
   
   public static class SumTable {
     private int[] sums;
+    private int[][] sums2d;
 
     public SumTable(int[] sums) {
       int n = sums.length;
@@ -24,6 +25,31 @@ public class SumTables {
         this.sums[i] = this.sums[i - 1] + sums[i];
       }
     }
+
+    public SumTable(int[][] sums) {
+      int n = sums.length;
+      if (n < 1) {
+        throw new UnsupportedOperationException("Array cannot be empty.");
+      }
+      int m = sums[0].length;
+      this.sums2d = new int[n][m];
+      for (int i = 0; i < n; i++) {
+        if (i < 1) {
+          this.sums2d[i][0] = sums[i][0];
+          for (int j = 1; j < m; j++) {
+            this.sums2d[i][j] = this.sums2d[i][j - 1] + sums[i][j];
+          }
+        } else {
+          this.sums2d[i][0] = sums[i][0] + this.sums2d[i - 1][0];
+          for (int j = 1; j < m; j++) {
+            this.sums2d[i][j] = sums[i][j] + 
+                this.sums2d[i][j - 1] + 
+                this.sums2d[i - 1][j] -
+                this.sums2d[i - 1][j - 1];
+          }
+        }
+      }
+    }
     
     public int sumForRange(int a, int b) {
       if (a < 0 || a >= sums.length || b < 0 || b >= sums.length) {
@@ -31,6 +57,11 @@ public class SumTables {
       }
       if (a == 0) return sums[b];
       return sums[b] - sums[a - 1];
+    }
+    
+    public int sumForRange(int ia, int ib, int ja, int jb) {
+      if (ia == 0 && ib == 0) return sums2d[ja][jb];
+      return sums2d[ja][jb] - sums2d[ja][ib] - sums2d[ia][jb] + sums2d[ia][ib];
     }
     
   }
@@ -41,6 +72,22 @@ public class SumTables {
     
     System.out.println("Sum between 0 and 5: " + st.sumForRange(0, 5));
     System.out.println("Sum between 5 and 10: " + st.sumForRange(5, 10));
+    
+    int[][] values2d = {
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1},
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1},
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1},
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1},
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1},
+      {1, 4, 5, 8, 3, 7, 1, 0, 6, 6, 2, 9, 2, 0, 4, 1}
+    };
+    SumTable st2d = new SumTable(values2d);
+    
+    System.out.println("Sum between (0,0) and (5,5): " + 
+        st2d.sumForRange(0, 0, 5, 5));
+    
+    System.out.println("Sum between (3,3) and (5,10): " + 
+        st2d.sumForRange(3, 3, 5, 10));
   }
   
 }
